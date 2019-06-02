7de2811e329424ebceeea2fb87262fcf075e7900
Daniel Pat <danielpat6@gmail.com>
2019-06-02 11:32:05 -0500

    working in cron taks

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const db = require('./models/database');
+require('./models/database');
 
 //Settings
 app.set('PORT', process.env.PORT || 5000)
