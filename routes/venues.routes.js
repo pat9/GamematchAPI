706aa19e99098fa4de679acd5a5f9d6f500d7c77
Daniel Pat <danielpat6@gmail.com>
2019-06-04 11:05:53 -0500

    working in venues and ads

--- routes/venues.routes.js
+++ routes/venues.routes.js
@@ -4,9 +4,25 @@ const router = express.Router();
 const Venues = require('../models/venues');
 
 //Get all the venues
-router.get('/', async(req, res) => {    
-    const venues = await Venues.find();
-    res.json(venues);    
+router.get('/', async(req, res) => {
+    const { lat, lng } = req.body;
+    if(lat != undefined && lng != undefined){
+        const venues = await Venues.aggregate([
+            {
+                "$geoNear": {
+                    near : { type: "Point", coordinates: [lng,lat] },
+                    distanceField: "dist.calculated",
+                    maxDistance: 500000, 
+                    includeLocs:"dist.location", 
+                    num: 10, spherical :true 
+                }
+            }
+        ]);
+        res.json(venues);        
+    }else{
+        const venues = await Venues.find();
+        res.json(venues);
+    }
 })
 
 //Get a single venue
