a7fd14ef6cda703cf86270e51c7a0d2ddef372e5
ElFozzy <y2jmontalvo@hotmail.com>
2019-07-05 18:20:08 -0500

    profile edit

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const  Auth  = require('../middleware/Auth')
 const users  = require('../models/user');
 const jwt = require('jsonwebtoken');
+const cloudinary = require('cloudinary').v2;
 
 router.use(Auth.IsLoggedIn)
 
@@ -12,11 +13,23 @@ router.get('/IsloggedIn', (req, res) =>{
 });
 
 
-router.put('/UpdateUser/:_id', async (req, res)=>{    
-    const { email, gametag, password, name, birthday, profilepic, description, facebookLink, twitterLink, twitchLink} = req.body;
-    const editUser = { email, gametag, password, name, birthday, profilepic, description, facebookLink, twitterLink, twitchLink};
+router.put('/UpdateUser/:_id', async (req, res)=>{   
+    const { email, gametag, password, name, birthday, description, facebookLink, twitterLink, twitchLink} = req.body;
+    const editUser = { email, gametag, password, name, birthday, description, facebookLink, twitterLink, twitchLink};
     const user = {_id:req.params._id, ...editUser}
     const token = jwt.sign({user}, process.env.TOKEN_SECRET_KEY, { expiresIn: '90h' });
+
+    let profilepic = {};
+    await cloudinary.uploader.upload(req.file.path, (error, result) => {
+        if(!error){
+            profilepic = result;
+            console.log(profilepic)
+        }
+        else{
+            console.log(error)
+            res.json({status:"Error in cloudinary"})
+        }
+    })
     await users.findByIdAndUpdate(req.params._id, editUser);
     res.json({status: 'Datos actualizados', token});
 });
