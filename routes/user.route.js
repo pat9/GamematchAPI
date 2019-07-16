35525991694282a15c5a01a3e942a2aa47c08dbd
ElFozzy <y2jmontalvo@hotmail.com>
2019-07-16 07:54:16 -0500

    update profilepic listo

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
-const  Auth  = require('../middleware/Auth')
+const Auth  = require('../middleware/Auth')
 const users  = require('../models/user');
 const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const path = require('path')
 const cloudinary = require('cloudinary').v2;
+
+const storage = multer.diskStorage({
+    destination:path.join(__dirname,'../temp/img'),
+    filename:(req,file,callback) =>{
+        callback(null, path.basename(file.originalname))
+    }
+})
+
+const upload = multer({storage})
+
 
 router.use(Auth.IsLoggedIn)
 
@@ -13,15 +25,18 @@ router.get('/IsloggedIn', (req, res) =>{
 });
 
 
-router.put('/UpdateUser/:_id', async (req, res)=>{   
+router.put('/UpdateUser/:_id', upload.single('profilepic'), async (req, res) => {   
     const { email, gametag, password, name, birthday, description, facebookLink, twitterLink, twitchLink} = req.body;
     const editUser = { email, gametag, password, name, birthday, description, facebookLink, twitterLink, twitchLink};
-    const user = {_id:req.params._id, ...editUser}
-    const token = jwt.sign({user}, process.env.TOKEN_SECRET_KEY, { expiresIn: '90h' });
-
+    
     let profilepic = {};
-    await cloudinary.uploader.upload(req.file.path, (error, result) => {
-        if(!error){
+    if (req.file != undefined){
+        await cloudinary.uploader.upload(req.file.path,  { type: "private",  
+        eager: [
+            { width: 200, crop: "scale" }, 
+        { width: 360, height: 200, 
+          crop: "crop", gravity: "north"} ] }, (error, result) => {
+              if(!error){
             profilepic = result;
             console.log(profilepic)
         }
@@ -30,8 +45,15 @@ router.put('/UpdateUser/:_id', async (req, res)=>{
             res.json({status:"Error in cloudinary"})
         }
     })
+}
+
+    const user = {_id:req.params._id, ...editUser, profilepic}
+    const token = jwt.sign({user}, process.env.TOKEN_SECRET_KEY, { expiresIn: '90h' });
     await users.findByIdAndUpdate(req.params._id, editUser);
     res.json({status: 'Datos actualizados', token});
+    
+    
+    
 });
 
 router.delete('/DeleteUser/:_id', async(req, res)=>{
