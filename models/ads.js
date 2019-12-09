47d89329ac8aef90c80619c9a70f3ecae1a3cc9e
Daniel Pat <danielpat6@gmail.com>
2019-12-09 11:23:59 -0600

    ads

--- models/ads.js
+++ models/ads.js
@@ -3,12 +3,8 @@ const mongoose = require('mongoose');
 const adsSchema = new mongoose.Schema({
     idVenue:{type:mongoose.SchemaTypes.ObjectId, required:true},
     titulo:{type:String, required:true},
-    descripcion:{type:String, required:true},
-    imagen:{type:String, required:false},
-    isCupon:{type:Boolean, required:true},
-    fechaCreacion:{type:Date, required:false, default:Date.now()},
-    fechaLimite:{type:Date, required:false, default:Date.now()},
-    numCupones:{type:Number, required:false},
+    imagen:{type:Object, required:false},
+    
 })
 
 module.exports = mongoose.model('ads', adsSchema);
--- routes/ads.routes.js
+++ routes/ads.routes.js
@@ -1,11 +1,23 @@
 const express = require ('express');
 const router = express.Router();
-
+const multer = require('multer');
+const path = require('path')
+const cloudinary = require('cloudinary').v2;
 const Ads = require('../models/ads');
 
-router.get("/", async (req, res) =>{
 
-    const { idVenue } = req.body;
+const storage = multer.diskStorage({
+    destination:path.join(__dirname,'../temp/img'),
+    filename:(req,file,callback) =>{
+        callback(null, path.basename(file.originalname))
+    }
+})
+
+const upload = multer({storage})
+
+router.get("/:id", async (req, res) =>{
+
+    const { idVenue } = req.params;
 
     if(idVenue != undefined){
         const ads = await Ads.find({idVenue});
@@ -13,13 +25,25 @@ router.get("/", async (req, res) =>{
     }
 
     const ads = await Ads.find();
+    console.log(ads)
     res.json(ads);
 })
 
-router.post('/create', async (req, res) =>{
+router.post('/create', upload.single('flyer'),async (req, res) =>{
     console.log(req.body)
-    const { idVenue, titulo, descripcion, isCupon, numCupones } = req.body;
-    const ad = new Ads({ idVenue, titulo, descripcion, isCupon, numCupones});
+    const { idVenue, titulo} = req.body;
+    let profilepic = {};
+    await cloudinary.uploader.upload(req.file.path, (error, result) => {
+        if(!error){
+            profilepic = result;
+            console.log(profilepic)
+        }
+        else{
+            console.log(error)
+            res.json({status:"Error in cloudinary"})
+        }
+    })
+    const ad = new Ads({ idVenue, titulo, imagen:profilepic});
     await ad.save();
     res.json("Listo")
 })
